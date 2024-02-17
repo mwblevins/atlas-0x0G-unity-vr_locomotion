7b236ab484edd38dcbc3ceae8a578c1fdedabbaf
mwblevins <79066736+mwblevins@users.noreply.github.com>
2024-02-16 18:06:03 -0600

    Shooting scripts

---
+++ Shooter/Assets/CustomScripts/FireBulletOnActivate.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+    public Animator gunAnimator;
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        gunAnimator.SetTrigger("Fire");
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+
+    }
+
+    public void ResetGunAnimation()
+    {
+        gunAnimator.ResetTrigger("Fire");
+    }
+}
